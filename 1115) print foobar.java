44bfda9b7c3bbdcdcb322eac57a2639eea15f838
hasi-sea <haashimcps147@gmail.com>
2026-07-27 13:33:04 +0530

    Create 1115) print foobar.java

---
+++ 1115) print foobar.java
@@ -0,0 +1,43 @@
+import java.util.concurrent.Semaphore;
+
+class FooBar {
+    private int n;
+    private final Semaphore fooSemaphore;
+    private final Semaphore barSemaphore;
+
+    public FooBar(int n) {
+        this.n = n;
+        
+        this.fooSemaphore = new Semaphore(1);
+        
+        this.barSemaphore = new Semaphore(0);
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+        
+            fooSemaphore.acquire();
+            
+            
+            printFoo.run();
+            
+          
+            barSemaphore.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            
+            barSemaphore.acquire();
+            
+           
+            printBar.run();
+            
+           
+            fooSemaphore.release();
+        }
+    }
+}
